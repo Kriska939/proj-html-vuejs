0d7940b070a47a0860c9cb032fa2c5246917718a
Kriska939 <cristinacorrado93@gmail.com>
2021-09-16 15:29:09 +0200

    added variables + complete jumbotron

--- src/App.vue
+++ src/App.vue
@@ -21,4 +21,5 @@ export default {
 </script>
 
 <style lang="scss">
+@import "@/assets/scss/style.scss";
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <section id="jumbotron" class="container-fluid">
+    <div class="row align-items-center text-center h-400">
+      <div class="col-12">
+        <h3>Commence Business</h3>
+        <h1 class="mb-4">Distant Mentoring Program</h1>
+        <button class="btn btn-outline-success btn-green-color">
+          Download Free Guidebook <i class="fas fa-arrow-right"></i>
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -8,5 +18,32 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+@import "@/assets/scss/_vars.scss";
+.h-400 {
+  height: 450px;
+}
+
+#jumbotron {
+  background-image: url("../assets/images/home-3-hero-bg.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+h1,
+h3 {
+  color: $white;
+}
+
+.btn-green-color {
+  background-color: $white;
+  color: $mountain-meadow;
+  border: 1px solid white;
+}
+
+.btn:hover {
+  background-color: $mountain-meadow;
+  color: $white;
+}
 </style>
