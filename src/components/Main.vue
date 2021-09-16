0dc07ca6829421617f60ccae48158dcac9cc0e6b
Kriska939 <cristinacorrado93@gmail.com>
2021-09-17 00:32:29 +0200

    late night update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,15 +12,15 @@
       </div>
     </section>
     <section class="infoBox mt-5 d-flex">
-      <div class="infoText me-5 pt-5">
+      <div class="infoTextType1 me-5 pt-5">
         <div class="mb-2">
           <h6 class="f-12 d-inline">EVERYTHING IN</h6>
           <span class="f-12 cinder fw-bold"> MAXCOACH</span>
         </div>
         <div class="mb-2">
-          <h3 class="d-inline">Learn about our</h3>
-          <span class="f-30 main-color"> Work Culture </span>
-          <h3 class="d-inline">at MaxCoach</h3>
+          <h2 class="d-inline fw-bold f-35">Learn about our</h2>
+          <span class="f-35 main-color"> Work Culture </span>
+          <h2 class="d-inline fw-bold f-35">at MaxCoach</h2>
         </div>
         <p class="description f-12">
           Spend some time to visit our website or head office and discover our
@@ -32,12 +32,16 @@
           >Get Free Guide <i class="fas fa-arrow-right"></i
         ></a>
       </div>
-      <div class="infoImg ps-5 pt-5">
-        <img src="../assets/images/people-infobox1.jpg" alt="Office" />
+      <div class="infoImgType1 pt-5">
+        <img
+          src="../assets/images/people-infobox1.jpg"
+          alt="Office"
+          class="img-fluid br-5"
+        />
         <img
           src="../assets/images/extra-img.jpg"
           alt="Office Lady"
-          class="extra-img"
+          class="extra-img br-5"
         />
       </div>
     </section>
@@ -56,6 +60,38 @@
       <div class="me-5">
         <h3 class="fw-bold f-30 main-color">4%</h3>
         <h6 class="f-12 fw-bold">SATISFACTION RATE</h6>
+      </div>
+    </section>
+    <section class="infoBox mt-5 mb-5 d-flex">
+      <div class="infoImgType2 pt-5">
+        <img
+          src="../assets/images/infobox-2.jpg"
+          alt="Office"
+          class="img-fluid br-5"
+        />
+        <img
+          src="../assets/images/icon-youtube-play.png"
+          alt="Youtube Icon"
+          class="youtube"
+        />
+      </div>
+      <div class="infoTextType2 ms-5 pt-5">
+        <div class="mb-2 mt-4">
+          <h6 class="f-12 d-inline fw-bold">HOW WE WORK</h6>
+        </div>
+        <div class="mb-2">
+          <h2 class="d-inline fw-bold f-35">Upgrade Your Skills</h2>
+          <span class="f-35 main-color"> Upgrade Your Life </span>
+        </div>
+        <p class="description f-12">
+          Opening up more opportunities in front of you to get the ticket to
+          more enjoyable door in life. Getting equipped with new skills for new
+          sets of insights into your career. <br />MaxCoach accompany learners
+          in every stage of learning.
+        </p>
+        <a href="#" class="f-12 main-color fw-bold"
+          >Download free guidebook <i class="fas fa-arrow-right"></i
+        ></a>
       </div>
     </section>
   </div>
@@ -100,18 +136,38 @@ h1,
   margin: 0 auto;
   max-height: 450px;
 
-  .infoText {
-    width: 30%;
-    height: 70%;
+  .infoTextType1 {
+    width: 40%;
+    height: 60%;
   }
 
-  .infoImg {
-    width: 70%;
+  .infoTextType2 {
+    width: 30%;
+    height: 60%;
+  }
+
+  .infoImgType1 {
+    width: 60%;
     background-image: url("../assets/images/maxcoach-shape-02.png"),
       url("../assets/images/maxcoach-shape-07-150x150.png");
     background-repeat: no-repeat;
     background-position: right bottom, center top;
     padding-bottom: 50px;
+    padding-right: 20px;
+    height: 70%;
+    position: relative;
+  }
+
+  .infoImgType2 {
+    width: 70%;
+    background-image: url("../assets/images/maxcoach-shape-07-150x150.png"),
+      url("../assets/images/maxcoach-shape-05-150x150.png"),
+      url("../assets/svg/svg-0.svg");
+    background-repeat: no-repeat;
+    background-position: right bottom, left center, left top;
+    padding-bottom: 40px;
+    padding-right: 20px;
+    padding-left: 70px;
     height: 70%;
     position: relative;
   }
@@ -120,7 +176,13 @@ h1,
     position: absolute;
     top: 0;
     transform: translateX(-70%);
-    border-radius: 5px;
+  }
+
+  .youtube {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-30%, -50%);
   }
 }
 </style>
